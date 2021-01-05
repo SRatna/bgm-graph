079e0940242ffa00c2995baeae33e757d44bd115
sratna <adhikariratna52@gmail.com>
2021-01-06 00:37:00 +0100

    made a basic hardcoded graph

---
+++ src/components/BGMGraph.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <svg height="500" width="500">
+      <circle
+        cx="250"
+        cy="250"
+        r="50"
+        fill="none"
+        stroke="black"
+        stroke-width="2"
+      />
+      <circle
+        cx="250"
+        cy="250"
+        r="100"
+        fill="none"
+        stroke="black"
+        stroke-width="2"
+      />
+      <circle
+        cx="250"
+        cy="250"
+        r="150"
+        fill="none"
+        stroke="black"
+        stroke-width="2"
+      />
+      <circle
+        cx="250"
+        cy="250"
+        r="200"
+        fill="none"
+        stroke="black"
+        stroke-width="2"
+      />
+      <line
+        x1="250"
+        y1="50"
+        x2="250"
+        y2="450"
+        fill="none"
+        stroke="black"
+        stroke-width="2"
+      />
+      <line
+        x1="50"
+        y1="250"
+        x2="450"
+        y2="250"
+        fill="none"
+        stroke="black"
+        stroke-width="2"
+      />
+      <line
+        x1="108"
+        y1="108"
+        x2="392"
+        y2="392"
+        fill="none"
+        stroke="black"
+        stroke-width="2"
+      />
+      <line
+        x1="392"
+        y1="108"
+        x2="108"
+        y2="392"
+        fill="none"
+        stroke="black"
+        stroke-width="2"
+      />
+      <text x="240" y="215" fill="black">1</text>
+      <text x="240" y="165" fill="black">2</text>
+      <text x="240" y="115" fill="black">3</text>
+      <text x="240" y="65" fill="black">4</text>
+      <circle
+        cx="108"
+        cy="108"
+        r="5"
+        fill="black"
+      />
+      <circle
+        cx="144"
+        cy="144"
+        r="5"
+        fill="black"
+      />
+      <circle
+        cx="180"
+        cy="180"
+        r="5"
+        fill="black"
+      />
+      <circle
+        cx="216"
+        cy="216"
+        r="5"
+        fill="black"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BGMGraph",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
