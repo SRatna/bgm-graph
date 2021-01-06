a40551b3f6edac363a8a147821cee6f254a1d725
sratna <adhikariratna52@gmail.com>
2021-01-06 01:44:48 +0100

    implemented functions to create graph components

--- src/components/BGMGraph.vue
+++ src/components/BGMGraph.vue
@@ -1,102 +1,36 @@
 <template>
   <div>
-    <svg height="500" width="500">
-      <circle
-        cx="250"
-        cy="250"
-        r="50"
-        fill="none"
-        stroke="black"
-        stroke-width="2"
-      />
-      <circle
-        cx="250"
-        cy="250"
-        r="100"
-        fill="none"
-        stroke="black"
-        stroke-width="2"
-      />
-      <circle
-        cx="250"
-        cy="250"
-        r="150"
-        fill="none"
-        stroke="black"
-        stroke-width="2"
-      />
-      <circle
-        cx="250"
-        cy="250"
-        r="200"
-        fill="none"
-        stroke="black"
-        stroke-width="2"
-      />
-      <line
-        x1="250"
-        y1="50"
-        x2="250"
-        y2="450"
-        fill="none"
-        stroke="black"
-        stroke-width="2"
-      />
-      <line
-        x1="50"
-        y1="250"
-        x2="450"
-        y2="250"
-        fill="none"
-        stroke="black"
-        stroke-width="2"
-      />
-      <line
-        x1="108"
-        y1="108"
-        x2="392"
-        y2="392"
-        fill="none"
-        stroke="black"
-        stroke-width="2"
-      />
-      <line
-        x1="392"
-        y1="108"
-        x2="108"
-        y2="392"
-        fill="none"
-        stroke="black"
-        stroke-width="2"
-      />
-      <text x="240" y="215" fill="black">1</text>
-      <text x="240" y="165" fill="black">2</text>
-      <text x="240" y="115" fill="black">3</text>
-      <text x="240" y="65" fill="black">4</text>
-      <circle
-        cx="108"
-        cy="108"
-        r="5"
-        fill="black"
-      />
-      <circle
-        cx="144"
-        cy="144"
-        r="5"
-        fill="black"
-      />
-      <circle
-        cx="180"
-        cy="180"
-        r="5"
-        fill="black"
-      />
-      <circle
-        cx="216"
-        cy="216"
-        r="5"
-        fill="black"
-      />
+    <svg :height="length" :width="length">
+      <template v-for="n in 4">
+        <circle
+          :cx="center"
+          :cy="center"
+          :r="distance90 * n"
+          fill="none"
+          stroke="black"
+          stroke-width="2"
+          :key="'circle' + n"
+        />
+      </template>
+      <template v-for="n in 8">
+        <line
+          :x1="center"
+          :y1="center"
+          :x2="getLinePoints(n, 4).x"
+          :y2="getLinePoints(n, 4).y"
+          fill="none"
+          stroke="black"
+          stroke-width="2"
+          :key="'line' + n"
+        />
+      </template>
+      <template v-for="n in 4">
+        <text :x="labelX" :y="labelY(n)" fill="black" :key="'label' + n">
+          {{ n }}
+        </text>
+      </template>
+      <!-- 
+      <circle cx="108" cy="108" r="5" fill="black" /> -->
     </svg>
   </div>
 </template>
@@ -104,6 +38,82 @@
 <script>
 export default {
   name: "BGMGraph",
+  data: function () {
+    return {
+      length: 500,
+      distance90: 50,
+      labelXOffset: 12, // this depends on text size
+      labelYOffset: 18,
+    };
+  },
+  computed: {
+    center() {
+      return this.length / 2;
+    },
+    distance45() {
+      return this.distance90 / Math.sqrt(2);
+    },
+    labelX() {
+      return this.center - this.labelXOffset;
+    },
+  },
+  methods: {
+    getLinePoints(lineNum, pointNum) {
+      switch (lineNum) {
+        case 1 || "one": {
+          return {
+            x: this.center + this.distance90 * pointNum,
+            y: this.center,
+          };
+        }
+        case 2 || "two": {
+          return {
+            x: this.center + this.distance45 * pointNum,
+            y: this.center - this.distance45 * pointNum,
+          };
+        }
+        case 3 || "three": {
+          return {
+            x: this.center,
+            y: this.center - this.distance90 * pointNum,
+          };
+        }
+        case 4 || "four": {
+          return {
+            x: this.center - this.distance45 * pointNum,
+            y: this.center - this.distance45 * pointNum,
+          };
+        }
+        case 5 || "five": {
+          return {
+            x: this.center - this.distance90 * pointNum,
+            y: this.center,
+          };
+        }
+        case 6 || "six": {
+          return {
+            x: this.center - this.distance45 * pointNum,
+            y: this.center + this.distance45 * pointNum,
+          };
+        }
+        case 7 || "seven": {
+          return {
+            x: this.center,
+            y: this.center + this.distance90 * pointNum,
+          };
+        }
+        case 8 || "eight": {
+          return {
+            x: this.center + this.distance45 * pointNum,
+            y: this.center + this.distance45 * pointNum,
+          };
+        }
+      }
+    },
+    labelY(pointNum) {
+      return this.center - pointNum * this.distance90 + this.labelYOffset;
+    },
+  },
 };
 </script>
 
